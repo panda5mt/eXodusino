8de17f2ec3e91b70061b801cc57d97d3adca2ef6
nucho <javatea@it.willcomlive.jp>
2012-03-17 17:09:53 +0900

    Added Print class

---
+++ src/core/Print.h
@@ -0,0 +1,40 @@
+/*
+ Print.cpp
+ Base class that provides print() and println()
+ Created on: 2012/03/17
+ */
+#ifndef Print_h
+#define Print_h
+
+#include "LPC11xx.h"
+
+const char radix_bin[] = "01";
+const char radix_oct[] = "01234567";
+const char radix_dec[] = "0123456789";
+const char radix_hex[] = "0123456789ABCDEF";
+
+typedef enum {
+	BYTE = 0x01, /*!< 1バイトのアスキーコードが表示される */
+	BIN = 0x02, /*!< dataで与えられた数値を2進数で表示する */
+	OCT = 0x08, /*!< dataで与えられた数値を8進数で表示する */
+	DEC = 0x0A, /*!< dataで与えられた数値を10進数で表示する */
+	HEX = 0x10
+/*!< dataで与えられた数値を16進数で表示する */
+} RADIX_FORMAT;
+
+
+class Print {
+private:
+	//	  	RADIX_FORMAT format;
+public:
+	virtual void write(const char c) = 0;
+	void write(const char *s,int length);
+
+	virtual void println(void);
+	void print(const char *s);
+	void print(int val, RADIX_FORMAT format);
+	void println(const char *s);
+	void println(int val, RADIX_FORMAT format);
+};
+
+#endif
